bc60cd63594822e7b6992f7ba90920ecafc84508
asourang <42590201+asourang@users.noreply.github.com>
2021-01-04 22:05:09 -0500

    commit message

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,Image, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:96}}>Profile</Text>
-      <Text style={{fontSize:40}}>Name: Aida</Text>
-      <Text style={{fontSize:40}}>Address: Sterling, VA</Text>
-      <Text style={{fontSize:40}}>Education: BS in Computer Science</Text>
+      <Image 
+      
+      source={
+    
+        require("./assets/me.png")
+        
+      
+      }
+      />
+      <Text style={{fontSize:22}}>Name: Aida</Text>
+      <Text style={{fontSize:22}}>Address: Sterling, VA</Text>
+      <Text style={{fontSize:22}}>Education: BS in Computer Science</Text>
       <StatusBar style="auto" />
     </View>
   );
